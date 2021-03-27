a063236a41ffa7a2bb376a66ed3126f3060c4b95
Jeongbin Park <pjb7687@gmail.com>
2021-03-27 14:11:09 +0100

    update comment

--- .ci/retrieve_readme.sh
+++ .ci/retrieve_readme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd src
 wget https://raw.githubusercontent.com/gnudatalanguage/gdl/master/README.md -O index.md
-content=`tail -n+6 index.md` # remove first 6 lines
+content=`tail -n+6 index.md` # remove first 5 lines
 printf "$content" > index.md
